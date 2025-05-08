981b1e26f592b0ebed5a9e3e2d7b8f382b089394
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-08 16:18:52 -0300

    feat: add youtube icon in header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import { Youtube } from "@deemlol/next-icons";
 
 export default function Header() {
   return (
     <header className="flex h-20 items-center py-20 px-50">
-      <div className="flex w-1/2 h-10 border-b-secondary border-b-1 justify-evenly text-xl header-links">
-        <Link href="/">Home</Link>
-        <Link href="/portfolio">Portfolio</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/portfolio">Youtube</Link>
+      <div className="flex w-1/2 h-10 border-b-secondary border-b-1 items-center justify-evenly text-xl header-links">
+        <Link href="/">
+          <span>Home</span>
+        </Link>
+        <Link href="/portfolio">
+          <span>Portfolio</span>
+        </Link>
+        <Link href="/blog">
+          <span>Blog</span>
+        </Link>
+        <Link href="/portfolio" className="flex items-center">
+          <span className="mr-1.5">Youtube</span>
+          <Youtube size={24} color="#f03" />
+        </Link>
       </div>
     </header>
   );
