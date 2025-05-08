83302bada4ddd79c4c5b73361489628681decc7d
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-08 11:47:58 -0300

    feat: add trusted remote URL to AWS S3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://mp-general-bucket.s3.sa-east-1.amazonaws.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
