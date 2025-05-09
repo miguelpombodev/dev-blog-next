81daf4d0727b5cc6db4818d3984b2804bb5f9864
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-09 16:36:58 -0300

    feat: add reponsive in experiences section

--- src/app/home/sections/experiencies.tsx
+++ src/app/home/sections/experiencies.tsx
@@ -7,16 +7,16 @@ export default async function ExperiencesSection() {
 
   return (
     <div className="flex bg-gray-light py-11 items-center justify-center flex-col">
-      <span className="text-center w-1/3 text-5xl/tight pb-4 border-b-1 border-black">
+      <span className="text-center w-4/5 text-5xl/tight pb-4 border-b-1 border-black md:w-1/3">
         Trusted with a variety of more experiences
       </span>
-      <span className="text-center w-3xl pt-11">
+      <span className="text-center px-10 pt-11 md:w-3xl md:px-0">
         With over 6 years of experience specializing in backend development with
         .NET (C#), with additional exposure to NodeJS and Java. I have a lot of
         expertise about building scalable applications, and integration with
         other tools, like RabbitMQ, Kafka Apache, Observability tools.
       </span>
-      <div className="flex py-11 border-y-1 border-black w-5/6 px-3 mt-11 justify-evenly">
+      <div className="flex flex-col py-5 border-y-1 border-black w-5/6 px-3 mt-11 justify-evenly md:flex-row md:py-11">
         <span className="flex flex-col text-center px-8">
           <p className="text-5xl">
             <span className="text-secondary">{getYearOfExperienceDiff()}</span>{" "}
@@ -24,7 +24,7 @@ export default async function ExperiencesSection() {
           </p>
           <p className="text-lg font-light">Experience</p>
         </span>
-        <span className="flex flex-col text-center px-30 border-x-1 border-black">
+        <span className="flex flex-col text-center my-5 py-5 border-y-1 border-black md:my-0 md:border-y-0 md:py-0 md:px-30 md:border-x-1 md:border-black">
           <p className="text-5xl">
             <span className="text-secondary">{res.public_repos}</span> repos
           </p>
