c0e2b304a89cb3a602c73b2d450fe48e38859b72
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-17 14:29:41 -0300

    feat: add tag component in article

--- src/app/components/Article/index.tsx
+++ src/app/components/Article/index.tsx
@@ -3,6 +3,7 @@ import { Pen } from "lucide-react";
 import ImageComponent from "../Image";
 import Link from "next/link";
 import { tranformTimezoneDateInLiteral } from "@/util/dates.util";
+import TagComponent from "../Tags";
 
 export default function ArticleComponent(props: ArticleComponentProps) {
   return (
@@ -17,18 +18,23 @@ export default function ArticleComponent(props: ArticleComponentProps) {
           title="article-image"
           className="w-18 h-18 rounded-[10] md:w-30 md:h-30"
         />
-        <div className="flex h-full flex-col justify-between gap-2">
+        <div className="flex h-full w-full flex-col justify-between gap-2">
           <h2 className="font-bold text-2xl">{props.article.title}</h2>
           <div className="flex flex-1">
             <h3 className="font-extralight text-sm text-gray-700">
               {props.article.briefDescription}
             </h3>
           </div>
-          <div className="flex">
+          <div className="flex justify-between">
             <span className="text-xs text-gray-700 flex gap-1 items-center">
               <Pen width={12} height={12} />
               {tranformTimezoneDateInLiteral(props.article.createdAt)}
             </span>
+            <div className="flex">
+              {props.article.tags.map((tag) => (
+                <TagComponent key={tag} name={tag} />
+              ))}
+            </div>
           </div>
         </div>
       </Link>
--- src/lib/interfaces/components/article.interface.ts
+++ src/lib/interfaces/components/article.interface.ts
@@ -3,3 +3,7 @@ import { IArticle } from "../http/article.interface";
 export interface ArticleComponentProps {
   article: IArticle;
 }
+
+export interface TagComponentProps {
+  name: string;
+}
