c0a968d83f3cc68dd6c52b80f59a6a258af17570
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-19 11:13:03 -0300

    fix: add nullable coalesce operator in Fetch Client Api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,16 +5,26 @@ type UrlsMapType = {
 
 export default class FetchApiClient {
   private readonly _cacheTime: number;
-  private readonly _urlsMap: UrlsMapType = {
-    github: process.env.NEXT_GITHUB_BASE_URL!,
-    devblog: process.env.NEXT_DEV_BLOG_API_URL!,
-  };
+
+  private getUrl(flag: keyof UrlsMapType): string {
+    const urlsMap: UrlsMapType = {
+      github: process.env.NEXT_GITHUB_BASE_URL ?? "",
+      devblog: process.env.NEXT_DEV_BLOG_API_URL ?? "",
+    };
+
+    if (!urlsMap[flag]) {
+      throw new Error(`Missing env variable for API base URL: ${flag}`);
+    }
+
+    return urlsMap[flag];
+  }
 
   constructor() {
     this._cacheTime = 60;
   }
   public async Get<T>(flag: keyof UrlsMapType, url: string): Promise<T> {
-    const res = await fetch(`${this._urlsMap[flag]}/${url}`, {
+    const baseUrl = this.getUrl(flag);
+    const res = await fetch(`${baseUrl}/${url}`, {
       next: { revalidate: this._cacheTime },
     });
 
